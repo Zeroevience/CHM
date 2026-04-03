7d473b3da65cdf3c318b6f0df1ba267486213bd1
Zeroevience <zeroevidence5@yahoo.com>
2026-04-03 05:38:18 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 
+local M = {}
+local lpm = loadstring(readfile("lpm/init.lua"))()
+lpm("install", "Zeroevience/ClientInfo")
+local client = lpm("require", "Zeroevience/ClientInfo")
+function M.speed(...)
+    local args=...
+    client.Character.Humanoid.Walkspeed = ...[1]
+    return "Done"
+end
+
+return M
